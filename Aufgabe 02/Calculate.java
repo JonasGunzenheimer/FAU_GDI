2be2cb598581145184802c79c2f57e4f68debc45
Jonas Gunzenheimer <153945790+JonasGunzenheimer@users.noreply.github.com>
2025-05-12 12:03:21 +0200

    Frag uns Aufgabe 02 angefangen.

---
+++ Aufgabe 02/Calculate.java
@@ -0,0 +1,33 @@
+/* 
+ * Erstellen Sie ein Programm in der öffentlichen Klasse Calculate. Legen Sie darin zwei Konstanten mit dem Datentypen double für eine Spannung (VOLTAGE) und eine Stromstärke (CURRENT) an. 
+ * Initialisieren Sie die Konstanten mit den unten angegebenen Werten. Legen Sie eine weitere Variable pwr mit dem Datentypen float an und speichern Sie darin die resultierende Leistung (Produkt aus VOLTAGE und CURRENT).
+
+Berechnen Sie außerdem die in 12 Stunden verbrauchte Energie (energy als float)  in kWh. Sie können dafür die folgende Formel verwenden:  energy=pwr∗time1000
+  .
+
+[Schwer] Geben Sie die Energie mit maximal zwei Nachkommastellen aus. Sie dürfen unnötige Stellen weglassen und müssen nicht korrekt runden. Verwenden Sie für die Bestimmung der formatierten Ausgabe weder Methodenaufrufe noch Kontrollstrukturen.
+
+Ihr Programm soll nun die Werte exakt wie unten abgebildet ausgeben. Verwenden Sie für die Ausgabe nur die entsprechenden Variablen. Legen Sie außer den vier oben angegebenen Variablen und Konstanten keine zusätzlichen an.
+
+    Spannung: 233.0V
+Stromstaerke: 9.1A
+
+    Leistung: 2120.3W
+ Energie 12h: 25.44kWh
+ */
+
+public class Calculate {
+    public static void main(String[] args) {
+
+        // 1. Konstanten anlegen
+
+        final double VOLTAGE = 223.0;
+        final double CURRENT = 9.1;
+
+        // Variable für Leistung
+
+        float pwr = (float) (VOLTAGE * CURRENT);
+        System.out.println("pwr:" + pwr);
+    }
+
+}
