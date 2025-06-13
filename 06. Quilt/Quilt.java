3110dceaa8bab282647e55b7b17a11964468bda8
Jonas Gunzenheimer <153945790+JonasGunzenheimer@users.noreply.github.com>
2025-06-13 17:23:58 +0200

    Unteraufgabe 3 abgeschlossen.

--- 06. Quilt/Quilt.java
+++ 06. Quilt/Quilt.java
@@ -20,11 +20,11 @@ public class Quilt {
         Manager quiltData = new Manager(args);
 
         // Farben auslesen 
-        int[] colorData = quiltData.getColorCount();
-        // System.out.println("color Data:" + colorData); // Test ob das gwünschte Objekt zurück gegeben wird
-        // System.out.println("Anzahl an verschiedenen Farben: " + colorData.length);
+        int[] initialColorData = quiltData.getColorCount();
+        // System.out.println("color Data:" + initialColorData); // Test ob das gwünschte Objekt zurück gegeben wird
+        // System.out.println("Anzahl an verschiedenen Farben: " + initialColorData.length);
         int i = 1;
-        for (int color : colorData) {
+        for (int color : initialColorData) {
             // System.out.println("Farbe Nr. " + i + " -> Verfuegbare Anzahl: " + color);
             i++;
         }
@@ -38,19 +38,28 @@ public class Quilt {
         int[][] quilt = new int[height][width]; 
         
         Quilt.showQuilt(quilt);
-        int randomColor = Quilt.randomColor(colorData); 
-        System.out.println("Erhaltene zufaellige Farbe: " + randomColor);
+        Quilt.initRandom(quilt, initialColorData);
+        System.out.println("Quilt wurde erstellt!");
+        Quilt.showQuilt(quilt);
+
+        // ! Testen über Manager Klasse 
+        Manager randomQuilt = new Manager(args);
+        System.out.println("Status: " + randomQuilt.testRandomQuilt(quilt));
     }
 
     public static void showQuilt(int[][] quilt) {
-        // quilt anzeigen 
+        // quilt anzeigen
         System.out.println();
         System.out.println("________");
         System.out.println("QUILT");
         System.out.println();
         for (int[] row : quilt) {
             for (int color : row) {
-                System.out.print("[" + color + "]");
+                if (color < 10) { // kein printf? 
+                    System.out.print("[ " + color + "]"); // einstellige Zahlen mit Leerzeichen
+                } else {
+                    System.out.print("[" + color + "]"); // zweistellige Zahlen normal
+                }
             }
             System.out.println();
         }
@@ -74,19 +83,62 @@ public class Quilt {
         // Verfügbare Farben in Array schreiben 
         
         int j = 0; 
-        System.out.print("Verfuegbare Farben: [");
+        // System.out.print("Verfuegbare Farben: [");
         for (int i = 0; i < colors.length; i++) {
             if (colors[i] > 0) {
-                validColors[j] = i + 1; 
-                System.out.print(validColors[j] + " ");
+                validColors[j] = i; 
+                // System.out.print(validColors[j] + " ");
                 j++; 
             }
         }
-        System.out.println("]");
+        // System.out.println("]");
 
-        int randi = (int) (Math.random() * (validColors.length + 1));
+        int randi = (int) (Math.random() * (validColors.length));
 
         // Aus einer der verfügbaren Farben auswählne und diese zurückgeben 
         return validColors[randi]; 
+    }
+
+    public static void initRandom(int[][] quilt, int[] initialColorData) {
+
+        // Kopie des Farb Arrays 
+        int[] colors = initialColorData; 
+        System.out.print("Farben Ausgangszustand: [");
+        int colorSum = 0; 
+        for (int color : colors) {
+            System.out.print(color + " ");
+            colorSum = colorSum + color; 
+        }
+        System.out.println("]");
+        System.out.println("Anzhal an verfuegbaren Karten zu Beginn: " + colorSum);
+
+        // quilt durchgehen und jedem platz eine verfügbare farbe zuteilen 
+        for (int row = 0; row < quilt.length; row++) {
+            for (int collum = 0; collum < quilt[row].length; collum++) {
+                // Random Color holen und an die stelle im quilt einsetzten 
+                int randColor = Quilt.randomColor(colors); 
+                quilt[row][collum] = randColor; 
+
+                // von der eingesetzten farbe -1 
+                colors[randColor] = colors[randColor] - 1; 
+                System.out.println("Gewaehlte Farbe: Nr. " + randColor);
+                System.out.print("Verfuegbare farben: [");
+                for (int color : colors) {
+                    System.out.print(color + " ");
+                }
+                System.out.println("]");
+
+            }
+        }
+        System.out.print("Farben Endzustand: [");
+        colorSum = 0; 
+        for (int color : colors) {
+            System.out.print(color + " ");
+            colorSum = colorSum + color;
+        }
+        System.out.println("]");
+        System.out.println("Anzhal an verfuegbaren Karten zu Ende: " + colorSum);
+
+
     }
 }
